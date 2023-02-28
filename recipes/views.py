91c1135cadb852e61181f7ab4139ade6998a3ec4
nadsoncley <nadson.cley@hotmail.com>
2023-02-28 08:20:40 -0400

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,11 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Receitas_Tradicionais',
     })
-
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
