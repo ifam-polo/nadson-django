0d15e90c3b1a2da44195cd7d9224ae03a70399fd
nadsoncley <nadson.cley@hotmail.com>
2023-02-28 08:47:22 -0400

    adicionado font-awesome-Libraries na home

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', context={
-        'name': 'Receitas_Tradicionais',
+        'name': 'Traditional',
     })
