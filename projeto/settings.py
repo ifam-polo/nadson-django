79eb0d55f144d678be6c0ba0bc9867d92b135833
nadsoncley <nadson.cley@hotmail.com>
2023-03-09 11:38:02 -0400

    corrigido arquivos home e head

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'recipes',
 ]
 
@@ -56,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'base_templates'
+            BASE_DIR / 'base_templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
