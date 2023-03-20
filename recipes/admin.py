6a942ad6f3da610bec8527e166dc9aae360a4512
nadsoncley <nadson.cley@hotmail.com>
2023-03-20 11:28:03 -0400

    Criado models, categorias, atributos e adminstrador django

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category
+
+
+# classe categoria criada para a classe administrativa do model.Ela herda da classe ModelAdmin.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
