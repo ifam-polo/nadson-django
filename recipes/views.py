b5ff83efbe9582d197167271474434e6a4d77e66
nadsoncley <nadson.cley@hotmail.com>
2023-03-23 11:55:39 -0400

    Ocultando receitas que não estão publicadas. Criado filtro para receitas publicadas ou não.

--- recipes/views.py
+++ recipes/views.py
@@ -7,18 +7,22 @@ from .models import Recipe  # Importação de recipes.models de Recipe.
 
 def home(request):
     # Busca todas receitas criada em ordem id (cadastro) investida.
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,  # Busca as receitas criadas em admin.
     })
 
 
+# Filtra os objetos de recipe por categoria pelo ID e pela ordem inversa.
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published=True,
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
-        # Define a quantidade de receitas por página.
+    return render(request, 'recipes/pages/category.html', context={
+        # Puxa as receitas cadastradas.
         'recipes': recipes,
     })
 
