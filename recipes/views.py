9c18b769cfc320192b1786d9abd55b9b2a8eab06
nadsoncley <nadson.cley@hotmail.com>
2023-03-24 11:50:37 -0400

    Corrigido category com Blanck True e default none, corrigindo autor nulo

--- recipes/views.py
+++ recipes/views.py
@@ -1,34 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
+from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
-
-from .models import Recipe  # Importação de recipes.models de Recipe.
 
 
 def home(request):
-    # Busca todas receitas criada em ordem id (cadastro) investida.
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,  # Busca as receitas criadas em admin.
-    })
-
-
-# Filtra os objetos de recipe por categoria pelo ID e pela ordem inversa.
-def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
-    ).order_by('-id')
-    return render(request, 'recipes/pages/category.html', context={
-        # Puxa as receitas cadastradas.
         'recipes': recipes,
     })
 
 
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
+    })
+
+
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
