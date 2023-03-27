3e417fbe8bcf9e52d4cb134552540dedf3aba7a1
nadsoncley <nadson.cley@hotmail.com>
2023-03-27 11:26:19 -0400

    Renomeado projeto inteiro e internacionalização, time zones e traduçãopara português PT-BR

--- projeto/asgi.py
+++ projeto/asgi.py
@@ -1,10 +1,10 @@
 """
-ASGI config for projeto project.
+ASGI config for projeto projeto.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+https://docs.djangoprojeto.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
--- projeto/wsgi.py
+++ projeto/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for projeto project.
+WSGI config for projeto projeto.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+https://docs.djangoprojeto.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
