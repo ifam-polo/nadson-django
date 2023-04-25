62c7105390ac5fa4da8c3d4092b575d86ae7b6eb
nadsoncley <nadson.cley@hotmail.com>
2023-04-25 11:29:42 -0400

    Criado testes funcionais para paginação com acessibilidade, testado o formulário de login, testado formulário de registro de autor.

--- utils/browser.py
+++ utils/browser.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from time import sleep
 
@@ -11,10 +12,12 @@ CHROMEDRIVER_PATH = ROOT_PATH / "bin" / CHROMEDRIVER_NAME
 
 def make_chrome_browser(*options):
     chrome_options = webdriver.ChromeOptions()
-
     if options is not None:
         for option in options:
             chrome_options.add_argument(option)
+
+    if os.environ.get("SELENIUM_HEADLESS") == "1":
+        chrome_options.add_argument("--headless")
 
     chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
     browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
@@ -25,3 +28,4 @@ if __name__ == "__main__":
     browser = make_chrome_browser("--headless")
     browser.get("http://www.udemy.com/")
     sleep(5)
+    browser.quit()
