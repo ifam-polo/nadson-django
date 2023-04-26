71766c63466f593cd78155bc538b4ea8fcff6007
nadsoncley <nadson.cley@hotmail.com>
2023-04-26 11:36:52 -0400

    Criado página inicial da dashboard, ajustado área administrativa padrão do Django e criado template, url e view para o usuário editar a receita.

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -156,19 +156,18 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         self.assertIn(msg, response.content.decode("utf-8"))
 
         def test_author_created_can_login(self):
-        url = reverse('authors:register_create')
+            url = reverse("authors:register_create")
 
-        self.form_data.update({
-            'username': 'testuser',
-            'password': '@Bc123456',
-            'password2': '@Bc123456',
-        })
+        self.form_data.update(
+            {
+                "username": "testuser",
+                "password": "@Bc123456",
+                "password2": "@Bc123456",
+            }
+        )
 
         self.client.post(url, data=self.form_data, follow=True)
 
-        is_authenticated = self.client.login(
-            username='testuser',
-            password='@Bc123456'
-        )
+        is_authenticated = self.client.login(username="testuser", password="@Bc123456")
 
         self.assertTrue(is_authenticated)
--- authors/views.py
+++ authors/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from recipes.models import Recipe
+
 from .forms import LoginForm, RegisterForm
 
 
@@ -50,7 +52,6 @@ def login_create(request):
     if not request.POST:
         raise Http404()
     form = LoginForm(request.POST)
-    login_url = reverse("authors:login")
     if form.is_valid():
         authenticated_user = authenticate(
             username=form.cleaned_data.get("username", ""),
@@ -63,7 +64,7 @@ def login_create(request):
             messages.error(request, "Invalid credentials")
     else:
         messages.error(request, "Invalid username or password")
-    return redirect(login_url)
+    return redirect(reverse("authors:dashboard"))
 
 
 @login_required(login_url="authors:login", redirect_field_name="next")
@@ -71,11 +72,35 @@ def logout_view(request):
     if not request.POST:
         messages.error(request, "Invalid logout request")
         return redirect(reverse("authors:login"))
-
     if request.POST.get("username") != request.user.username:
         messages.error(request, "Invalid logout user")
         return redirect(reverse("authors:login"))
-
     messages.success(request, "Logged out successfully")
     logout(request)
     return redirect(reverse("authors:login"))
+
+
+@login_required(login_url="authors:login", redirect_field_name="next")
+def dashboard(request):
+    recipes = Recipe.objects.filter(is_published=False, author=request.user)
+    return render(
+        request,
+        "authors/pages/dashboard.html",
+        context={
+            "recipes": recipes,
+        },
+    )
+
+
+@login_required(login_url="authors:login", redirect_field_name="next")
+def dashboard_recipe_edit(request, id):
+    recipe = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+        pk=id,
+    )
+
+    if not recipe:
+        raise Http404()
+
+    return render(request, "authors/pages/dashboard_recipe.html", context={})
