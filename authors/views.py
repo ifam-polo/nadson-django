0c43755ddff7f1f0b24a6c729616a69df360875e
nadsoncley <nadson.cley@hotmail.com>
2023-04-26 08:54:06 -0400

    Testado login inválido e testado usuário e senha inválidos.

--- authors/views.py
+++ authors/views.py
@@ -32,10 +32,8 @@ def register_create(request):
         user.set_password(user.password)
         user.save()
         messages.success(request, "Your user is created, please log in.")
-
         del request.session["register_form_data"]
         return redirect(reverse("authors:login"))
-
     return redirect("authors:register")
 
 
@@ -65,17 +63,19 @@ def login_create(request):
             messages.error(request, "Invalid credentials")
     else:
         messages.error(request, "Invalid username or password")
-
     return redirect(login_url)
 
 
 @login_required(login_url="authors:login", redirect_field_name="next")
 def logout_view(request):
     if not request.POST:
+        messages.error(request, "Invalid logout request")
         return redirect(reverse("authors:login"))
 
     if request.POST.get("username") != request.user.username:
+        messages.error(request, "Invalid logout user")
         return redirect(reverse("authors:login"))
 
+    messages.success(request, "Logged out successfully")
     logout(request)
     return redirect(reverse("authors:login"))
--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -10,25 +10,70 @@ from .base import AuthorsBaseTest
 class AuthorsLoginTest(AuthorsBaseTest):
     def test_user_valid_data_can_login_successfully(self):
         string_password = "pass"
-        user = User.objects.create_user(username="my_user", password=string_password)
+        user = User.objects.create_user(
+            username="my_user", password=string_password
+        )  # noqa: E501
+        # Usuário abre a página de login
+        self.browser.get(self.live_server_url + reverse("authors:login"))
+        # Usuário vê o formulário de login
+        form = self.browser.find_element(By.CLASS_NAME, "main-form")
+        username_field = self.get_by_placeholder(form, "Type your username")
+        password_field = self.get_by_placeholder(form, "Type your password")
+        # Usuário digita seu usuário e senha
+        username_field.send_keys(user.username)
+        password_field.send_keys(string_password)
+        # Usuário envia o formulário
+        form.submit()
+        # Usuário vê a mensagem de login com sucesso e seu nome
+        self.assertIn(
+            f"Your are logged in with {user.username}.",
+            self.browser.find_element(By.TAG_NAME, "body").text,
+        )
 
+    def test_login_create_raises_404_if_not_POST_method(self):
+        self.browser.get(
+            self.live_server_url + reverse("authors:login_create")
+        )  # noqa: E501
+        self.assertIn(
+            "Not Found", self.browser.find_element(By.TAG_NAME, "body").text
+        )  # noqa: E501
+
+    def test_form_login_is_invalid(self):
+        # Usuário abre a página de login
+        self.browser.get(self.live_server_url + reverse("authors:login"))
+        # Usuário vê o formulário de login
+        form = self.browser.find_element(By.CLASS_NAME, "main-form")
+        # E tenta enviar valores vazios
+        username = self.get_by_placeholder(form, "Type your username")
+        password = self.get_by_placeholder(form, "Type your password")
+        username.send_keys(" ")
+        password.send_keys(" ")
+        # Envia o formulário
+        form.submit()
+        # Vê uma mensagem de erro na tela
+        self.assertIn(
+            "Invalid username or password",
+            self.browser.find_element(By.TAG_NAME, "body").text,
+        )
+
+    def test_form_login_invalid_credentials(self):
         # Usuário abre a página de login
         self.browser.get(self.live_server_url + reverse("authors:login"))
 
         # Usuário vê o formulário de login
         form = self.browser.find_element(By.CLASS_NAME, "main-form")
-        username_field = self.get_by_placeholder(form, "Type your username")
-        password_field = self.get_by_placeholder(form, "Type your password")
 
-        # Usuário digita seu usuário e senha
-        username_field.send_keys(user.username)
-        password_field.send_keys(string_password)
+        # E tenta enviar valores com dados que não correspondem
+        username = self.get_by_placeholder(form, "Type your username")
+        password = self.get_by_placeholder(form, "Type your password")
+        username.send_keys("invalid_user")
+        password.send_keys("invalid_password")
 
-        # Usuário envia o formulário
+        # Envia o formulário
         form.submit()
 
-        # Usuário vê a mensagem de login com sucesso e seu nome
+        # Vê uma mensagem de erro na tela
         self.assertIn(
-            f"Your are logged in with {user.username}.",
-            self.browser.find_element(By.TAG_NAME, "body").text,
+            "Invalid credentials",
+            self.browser.find_element(By.TAG_NAME, "body").text,  # noqa: E501
         )
