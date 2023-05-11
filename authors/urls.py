2d05c27012d4a966619c8363b2a50d02047266c3
nadsoncley <nadson.cley@hotmail.com>
2023-05-11 11:11:39 -0400

    Adicionado login_required em class based views e seus métodos.

--- authors/urls.py
+++ authors/urls.py
@@ -1,4 +1,3 @@
-# flake8: noqa
 from django.urls import path
 
 from . import views
@@ -12,11 +11,13 @@ urlpatterns = [
     path("logout/", views.logout_view, name="logout"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path(
-        "dashboard/recipe/new/", views.dashboard_recipe_new, name="dashboard_recipe_new"
+        "dashboard/recipe/new/",
+        views.DashboardRecipe.as_view(),
+        name="dashboard_recipe_new",
     ),
     path(
         "dashboard/recipe/delete/",
-        views.dashboard_recipe_delete,
+        views.DashboardRecipeDelete.as_view(),
         name="dashboard_recipe_delete",
     ),
     path(
--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -1,28 +1,40 @@
 # flake8: noqa
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http.response import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import View
 
 from authors.forms.recipe_form import AuthorRecipeForm
 from recipes.models import Recipe
 
 
+@method_decorator(
+    login_required(login_url="authors:login", redirect_field_name="next"),
+    name="dispatch",
+)
 class DashboardRecipe(View):
-    def get_recipe(self, id):
-        recipe = None
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        if id:
+    def setup(self, *args, **kwargs):
+        return super().setup(*args, **kwargs)
+
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def get_recipe(self, id=None):
+        recipe = None
+        if id is not None:
             recipe = Recipe.objects.filter(
                 is_published=False,
                 author=self.request.user,
                 pk=id,
             ).first()
-
             if not recipe:
                 raise Http404()
-
         return recipe
 
     def render_recipe(self, form):
@@ -30,12 +42,12 @@ class DashboardRecipe(View):
             self.request, "authors/pages/dashboard_recipe.html", context={"form": form}
         )
 
-    def get(self, request, id):
+    def get(self, request, id=None):
         recipe = self.get_recipe(id)
         form = AuthorRecipeForm(instance=recipe)
         return self.render_recipe(form)
 
-    def post(self, request, id):
+    def post(self, request, id=None):
         recipe = self.get_recipe(id)
         form = AuthorRecipeForm(
             data=request.POST or None, files=request.FILES or None, instance=recipe
@@ -48,6 +60,18 @@ class DashboardRecipe(View):
             recipe.is_published = False
             recipe.save()
             messages.success(request, "Sua receita foi salva com sucesso!")
-            return redirect(reverse("authors:dashboard_recipe_edit", args=(id,)))
+            return redirect(reverse("authors:dashboard_recipe_edit", args=(recipe.id,)))
 
         return self.render_recipe(form)
+
+
+@method_decorator(
+    login_required(login_url="authors:login", redirect_field_name="next"),
+    name="dispatch",
+)
+class DashboardRecipeDelete(DashboardRecipe):
+    def post(self, *args, **kwargs):
+        recipe = self.get_recipe(self.request.POST.get("id"))
+        recipe.delete()
+        messages.success(self.request, "Deleted successfully.")
+        return redirect(reverse("authors:dashboard"))
