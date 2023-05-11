8dd46f61d97241dd51d63789b7f1d5485d5bb10d
nadsoncley <nadson.cley@hotmail.com>
2023-05-11 08:38:21 -0400

    Criado em models um slug auto preenchivel com  o pŕoprio título da receita.

--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.urls import reverse  # noqa: F401
-from django.utils.text import slugify  # noqa: F401
+from django.urls import reverse
+from django.utils.text import slugify
 
 
-# criação da classe categoria. Registro dos models.
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
@@ -28,7 +27,6 @@ class Recipe(models.Model):
     cover = models.ImageField(
         upload_to="recipes/covers/%Y/%m/%d/", blank=True, default=""
     )
-
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
@@ -39,4 +37,14 @@ class Recipe(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return self.title  # Apresenta título da receita.
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("recipes:recipe", args=(self.id,))
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f"{slugify(self.title)}"
+            self.slug = slug
+
+        return super().save(*args, **kwargs)
