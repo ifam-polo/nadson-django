69c94a44c13a42a42ad69e24d15b2e7df2d1292c
nadsoncley <nadson.cley@hotmail.com>
2023-05-12 11:25:04 -0400

    Criado um base para home, category e Search de recipes.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,13 +1,16 @@
+# flake8: noqa
 from django.urls import path
 
 from . import views
 
-app_name = 'recipes'
-
+app_name = "recipes"
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('recipes/search/', views.search, name="search"),
-    path('recipes/category/<int:category_id>/',
-         views.category, name="category"),
-    path('recipes/<int:id>/', views.recipe, name="recipe"),
+    path("", views.RecipeListViewHome.as_view(), name="home"),
+    path("recipes/search/", views.RecipeListViewSearch.as_view(), name="search"),
+    path(
+        "recipes/category/<int:category_id>/",
+        views.RecipeListViewCategory.as_view(),
+        name="category",
+    ),
+    path("recipes/<int:pk>/", views.RecipeDetail.as_view(), name="recipe"),
 ]
