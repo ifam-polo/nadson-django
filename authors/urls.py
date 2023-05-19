d34dadfa7f24d5676f99ab636473fe6b3cb710e0
nadsoncley <nadson.cley@hotmail.com>
2023-05-19 10:32:26 -0400

    Criado tamplete,view e url para profile.

--- authors/urls.py
+++ authors/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
         views.DashboardRecipe.as_view(),
         name="dashboard_recipe_edit",
     ),
+    path("profile/<int:id>/", views.ProfileView.as_view(), name="profile"),
 ]
