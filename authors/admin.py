024bf92687a2c691a1b4e2d5617f7121ddde00bd
nadsoncley <nadson.cley@hotmail.com>
2023-05-19 09:52:05 -0400

    Criado o model de perfil de usuário(profile).

--- authors/admin.py
+++ authors/admin.py
@@ -1,4 +1,9 @@
 # flake8: noqa
 from django.contrib import admin
 
-# Register your models here.
+from authors.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    ...
